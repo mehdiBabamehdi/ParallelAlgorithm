99c3ddd9b609576629a5ea4575f92dabf1297f99
mehdiBabamehdi <m.babamehdi@gmail.com>
2019-06-25 09:21:37 +0200

    add time calculation

--- alg20_2.c
+++ alg20_2.c
@@ -14,28 +14,27 @@
 
 int main()
 {
-   int numb_stage, arrayDim;
+   int numb_stage, array_dim;
    float *rand_numb; 
    float sum =0;  
-   clock_t start_t;
- 
-   printf("Enter number of stage, the total number of data would be 2^stage\n");
-   printf("numb_stage = "); 
-   scanf("%d", &numb_stage);
-   arrayDim = pow(2,numb_stage);
 
-   printf("numb_stageumber of data = %d \n", arrayDim);
+   struct timespec start_t ={0,0} , end_t={0,0};
+   printf("Enter number of stage, the total number of data would be 2^stage\n");
+   printf("stage number = "); 
+   scanf("%d", &numb_stage);
+   array_dim = pow(2,numb_stage);
+
+   printf("number of data = %d \n", array_dim);
    rand_numb = (float*) malloc(numb_stage * sizeof(float));
    
    // initializing "rand_numb" with random nunber bewteen 0-19 and 
-   for (int i=0; i<arrayDim; i++)
+   for (int i=0; i<array_dim; i++)
    {
       *(rand_numb+i) = rand() % 20;
       sum += *(rand_numb+i);
-    //  printf("rand_numb%d= %f\n",i,*(a+i));
    }
    
-   start_t = clock();
+   clock_gettime(CLOCK_MONOTONIC,&start_t);
    for (int k=0; k<=numb_stage; k++)
    {
          for (int i=0; i<pow(2,(numb_stage-k)); i++)
@@ -44,7 +43,8 @@ int main()
      }	     
    }
 
-   printf("Operation time= %f\n",(float)((clock() - start_t) / CLOCKS_PER_SEC));
+   clock_gettime(CLOCK_MONOTONIC,&end_t);
+   printf("Operation time= %f nsec\n",(( double)((end_t.tv_sec + 1.0e-9*end_t.tv_nsec) - (double) (start_t.tv_sec - 1.0e-9*start_t.tv_nsec))));
    printf("result= %f\n", *rand_numb);
    printf("result (correct)= %f\n",sum);
    
