b9e618cdd2c392bd3cd01438c06663fef8fa083e
Gustavo <tavroot@gmail.com>
2019-06-30 08:24:23 +0200

    Soft dev re-structure of the C and MPI-C implementations
    Main changes:
    
    * added .h files through .gitignore
    * expanded Makefile to separate tests from implementations,
      but has to be extended further for cross-dependencies
    * moved alg20_2 to an impl function, and called it from tests

---
+++ algorithms/c/impl_alg20_2s.h
@@ -0,0 +1,11 @@
+#ifndef IMPL_ALG_20_2_SEQ
+  #define IMPL_ALG_20_2_SEQ
+
+  #include <stdlib.h>
+  #include <stdio.h>
+  #include <math.h>
+  #include <time.h>
+
+  void fan_in_sum_seq();
+
+#endif
